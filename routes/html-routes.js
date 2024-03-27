88d77ce17f1f80d272f78c818b6a4332a598c926
JacksonByrd7 <jackson.byrd7@gmail.com>
2024-03-26 20:12:23 -0400

    html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,12 @@
-const router = require('express').router();
+const router = require('express').Router();
 const path = require('path');
 
 //defines the route that sends 'index.html' as a response to a client when a GET request is made
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
-
+//defines the route that sends 'notes.html' as a response to a client when a GET request is made
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(_dirname, '../public/notes.html'))
+});
 module.exports = router;
